9d05c163622928ccf8d92b77614c8405edc3eb58
Ivan Post <ivan.post24@gmail.com>
2025-11-21 09:53:10 -0500

    Removed redundant include_directories and made other minor linking improvements

--- examples/2d_detect.cpp
+++ examples/2d_detect.cpp
@@ -3,10 +3,10 @@
 //
 
 #include <iostream>
+#include <tagStandard41h12.h>
 #include <opencv2/opencv.hpp>
 
 #include "overlays.hpp"
-#include "tagStandard41h12.h"
 #include "wrapper.hpp"
 
 int main(const int argc, const char* argv[]) {
